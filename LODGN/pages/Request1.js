ef59618374a5b02d004f5c7bd30652af1a3391d5
fizza897 <99113832+fizza897@users.noreply.github.com>
2023-04-02 22:31:25 +0500

    Add files via upload

---
+++ LODGN/pages/Request1.js
@@ -0,0 +1,213 @@
+const Request1 = () => {
+  return (
+    <div className="relative bg-white w-full h-[1117px] overflow-hidden text-left text-base-1 text-darkgray font-lato">
+      <div className="absolute top-[0px] left-[0px] bg-gainsboro w-[1728px] h-[1117px]" />
+      <div className="absolute top-[75.71px] left-[394px] text-[28.56px] tracking-[-0.02em] font-semibold font-poppins text-darkseagreen">
+        You currently have 3 requests
+      </div>
+      <div className="absolute top-[159px] left-[402px] bg-white shadow-[2px_2px_8px_rgba(0,_0,_0,_0.25)] box-border w-[1114px] h-[226px] border-[1px] border-solid border-white" />
+      <div className="absolute top-[445px] left-[395px] rounded-[54px] bg-white shadow-[0px_4px_8px_rgba(0,_0,_0,_0.25)] box-border w-[1116px] h-[528px] border-[1px] border-solid border-white" />
+      <div className="absolute top-[206px] left-[441px] w-[708px] h-[58.7px] text-[16.58px]">
+        <b className="absolute top-[1px] left-[0px] text-10xl-4 tracking-[-0.01em] text-mediumseagreen">{`St Judes Hospital   `}</b>
+        <div className="absolute top-[34px] left-[0px] text-[16.93px] font-medium">
+          Sarasota,FL. 33178
+        </div>
+        <div className="absolute top-[34px] left-[310px] font-medium">
+          October
+        </div>
+        <div className="absolute top-[34px] left-[403px] font-medium">
+          December
+        </div>
+        <div className="absolute top-[34px] left-[540px] text-[17.58px] font-medium">
+          {`10 Singles, 10 `}
+          <span className="tracking-[-0.04em]">doubles</span>
+        </div>
+        <b className="absolute top-[0px] left-[310px] text-[27.94px] tracking-[-0.02em] text-mediumseagreen">
+          10 - 17
+        </b>
+        <b className="absolute top-[0px] left-[540px] text-[29.64px] tracking-[-0.02em] text-mediumseagreen">
+          20 Rooms
+        </b>
+        <img
+          className="absolute top-[3.29px] left-[250px] w-[253.16px] h-[55.41px]"
+          alt=""
+          src="/line-10.svg"
+        />
+      </div>
+      <div className="absolute top-[480px] left-[443px] w-[708px] h-[59.09px] text-[16.58px]">
+        <b className="absolute top-[2.99px] left-[0px] text-10xl-4 tracking-[-0.01em] text-mediumseagreen">{`St Judes Hospital   `}</b>
+        <div className="absolute top-[36.06px] left-[0px] text-[16.93px] font-medium">
+          Sarasota,FL. 33178
+        </div>
+        <div className="absolute top-[33.78px] left-[292px] font-medium">
+          October
+        </div>
+        <div className="absolute top-[33.78px] left-[390.97px] font-medium">
+          December
+        </div>
+        <div className="absolute top-[33.49px] left-[540px] text-[17.58px] font-medium">
+          {`10 Singles, 10 `}
+          <span className="tracking-[-0.04em]">doubles</span>
+        </div>
+        <b className="absolute top-[1.39px] left-[292.2px] text-[27.94px] tracking-[-0.02em] text-mediumseagreen">
+          10 - 17
+        </b>
+        <b className="absolute top-[0px] left-[538.69px] text-[29.64px] tracking-[-0.02em] text-mediumseagreen">
+          20 Rooms
+        </b>
+        <img
+          className="absolute top-[3.68px] left-[250px] w-[253.16px] h-[55.41px]"
+          alt=""
+          src="/line-101.svg"
+        />
+      </div>
+      <div className="absolute top-[294px] left-[414px] rounded-tl-3xl rounded-tr-none rounded-br-none rounded-bl-3xl bg-yellow box-border w-[387px] h-12 border-[1px] border-solid border-yellow" />
+      <div className="absolute top-[575px] left-[437px] rounded-3xl bg-seagreen w-[712px] h-12" />
+      <div className="absolute top-[306px] left-[437px] text-[16.93px] tracking-[-0.07em] font-medium">
+        RECEIVED
+      </div>
+      <div className="absolute top-[307px] left-[881px] tracking-[-0.07em] font-medium">
+        NEGOTIATING
+      </div>
+      <div className="absolute top-[307px] left-[1226px] tracking-[-0.07em] font-medium">
+        COMPLETED
+      </div>
+      <div className="absolute top-[588px] left-[468px] tracking-[-0.07em] font-medium text-white">
+        COMPLETED
+      </div>
+      <div className="absolute top-[293.5px] left-[1056.5px] box-border w-px h-[49px] border-r-[1px] border-solid border-darkgray" />
+      <div className="absolute top-[293.5px] left-[800.5px] box-border w-px h-[49px] border-r-[1px] border-solid border-darkgray" />
+      <div className="absolute top-[0px] left-[0px] w-[296px] h-[1117px] text-3xl text-darkslategray-100 font-poppins">
+        <div className="absolute top-[0px] left-[0px] bg-white shadow-[2px_0px_10px_rgba(0,_0,_0,_0.25)] w-[296px] h-[1117px]" />
+        <i className="absolute top-[37.08px] left-[38px] text-[35.48px] font-extrabold font-lato text-darkslategray-200">
+          LODGN
+        </i>
+        <div className="absolute top-[146.95px] left-[19px] text-[22.8px] tracking-[-0.02em] font-semibold text-darkseagreen">
+          Current requests
+        </div>
+        <div className="absolute top-[227px] left-[19px] tracking-[-0.02em] font-semibold">
+          Ongoing stays
+        </div>
+        <div className="absolute top-[306px] left-[19px] tracking-[-0.02em] font-semibold">
+          Previous stays
+        </div>
+        <div className="absolute top-[385px] left-[19px] tracking-[-0.02em] font-semibold">
+          Reports
+        </div>
+        <div className="absolute top-[884px] left-[29px] rounded-3xs [background:linear-gradient(180deg,_#48af78,_#42996b)] w-[231px] h-[70px]" />
+        <div className="absolute top-[904px] left-[99px] tracking-[-0.05em] font-semibold text-white">
+          Log - Out
+        </div>
+        <img
+          className="absolute top-[200px] left-[19.5px] w-[249.5px] h-px"
+          alt=""
+          src="/line-14.svg"
+        />
+        <img
+          className="absolute top-[281.5px] left-[19.5px] w-[249.5px] h-px"
+          alt=""
+          src="/line-15.svg"
+        />
+        <img
+          className="absolute top-[361px] left-[19.5px] w-[249.5px] h-px"
+          alt=""
+          src="/line-14.svg"
+        />
+        <img
+          className="absolute top-[441.5px] left-[19.5px] w-[249.5px] h-px"
+          alt=""
+          src="/line-15.svg"
+        />
+        <div className="absolute top-[966px] left-[108px] text-[17.59px] tracking-[-0.07em] font-medium font-lato">
+          Help-Desk:
+        </div>
+        <div className="absolute top-[992px] left-[98px] text-[17.59px] tracking-[-0.07em] font-medium font-lato">
+          786-874 9988
+        </div>
+      </div>
+      <div className="absolute top-[685px] left-[820px] w-[275px] h-[220px] text-[17.59px] font-poppins">
+        <div className="absolute top-[0px] left-[0px] rounded-3xs bg-whitesmoke-100 box-border w-[275px] h-[220px] border-[1px] border-solid border-gray" />
+        <i className="absolute top-[85px] left-[88px] text-[19.12px] tracking-[-0.01em] text-lightgreen">
+          Holiday Inn
+        </i>
+        <div className="absolute top-[127px] left-[0px] rounded-3xs bg-whitesmoke-200 shadow-[0px_-2px_8px_rgba(0,_0,_0,_0.25)] box-border w-[275px] h-[93px] border-[1px] border-solid border-gray" />
+        <img
+          className="absolute top-[19.5px] left-[113.5px] w-[53px] h-[58px]"
+          alt=""
+          src="/vector-1.svg"
+        />
+        <div className="absolute top-[139.12px] left-[23px] tracking-[-0.07em] font-medium font-lato">{`1.5 miles away from joblocation. `}</div>
+        <div className="absolute top-[170.74px] left-[15px] text-[15.23px] tracking-[-0.07em] font-medium">
+          Singles: $120
+        </div>
+        <div className="absolute top-[188px] left-[15px] text-[15.23px] tracking-[-0.07em] font-medium">
+          Doubles: $145
+        </div>
+        <div className="absolute top-[171px] left-[138px] rounded-3xs bg-seagreen w-[126px] h-10" />
+        <div className="absolute top-[180px] left-[164px] tracking-[-0.07em] font-medium font-lato text-white">
+          Book now
+        </div>
+      </div>
+      <div className="absolute top-[685px] left-[1198px] w-[275px] h-[220px] text-[17.59px] font-poppins">
+        <div className="absolute top-[0px] left-[0px] rounded-3xs bg-whitesmoke-100 box-border w-[275px] h-[220px] border-[1px] border-solid border-gray" />
+        <i className="absolute top-[85px] left-[88px] text-[19.12px] tracking-[-0.01em] text-lightgreen">
+          Holiday Inn
+        </i>
+        <div className="absolute top-[127px] left-[0px] rounded-3xs bg-whitesmoke-200 shadow-[0px_-2px_8px_rgba(0,_0,_0,_0.25)] box-border w-[275px] h-[93px] border-[1px] border-solid border-gray" />
+        <img
+          className="absolute top-[19.5px] left-[113.5px] w-[53px] h-[58px]"
+          alt=""
+          src="/vector-1.svg"
+        />
+        <div className="absolute top-[139.12px] left-[23px] tracking-[-0.07em] font-medium font-lato">{`1.5 miles away from joblocation. `}</div>
+        <div className="absolute top-[170.74px] left-[15px] text-[15.23px] tracking-[-0.07em] font-medium">
+          Singles: $120
+        </div>
+        <div className="absolute top-[188px] left-[15px] text-[15.23px] tracking-[-0.07em] font-medium">
+          Doubles: $145
+        </div>
+        <div className="absolute top-[171px] left-[138px] rounded-3xs bg-seagreen w-[126px] h-10" />
+        <div className="absolute top-[180px] left-[164px] tracking-[-0.07em] font-medium font-lato text-white">
+          Book now
+        </div>
+      </div>
+      <div className="absolute top-[664px] left-[437px] w-80 h-[241px] text-[17.59px] font-poppins">
+        <div className="absolute top-[0px] left-[0px] w-80 h-[241px]">
+          <div className="absolute top-[20px] left-[-1px] rounded-3xs bg-whitesmoke-100 box-border w-[277px] h-[222px] border-[1px] border-solid border-gray" />
+          <i className="absolute top-[106px] left-[88px] text-[19.12px] tracking-[-0.01em] text-lightgreen">
+            Holiday Inn
+          </i>
+          <div className="absolute top-[148px] left-[0px] rounded-3xs bg-whitesmoke-200 shadow-[0px_-2px_8px_rgba(0,_0,_0,_0.25)] box-border w-[275px] h-[93px] border-[1px] border-solid border-gray" />
+          <img
+            className="absolute top-[40.5px] left-[113.5px] w-[53px] h-[58px]"
+            alt=""
+            src="/vector-1.svg"
+          />
+          <div className="absolute top-[160.12px] left-[23px] tracking-[-0.07em] font-medium font-lato">{`1.5 miles away from joblocation. `}</div>
+          <div className="absolute top-[191.74px] left-[15px] text-[15.23px] tracking-[-0.07em] font-medium">
+            Singles: $120
+          </div>
+          <div className="absolute top-[209px] left-[15px] text-[15.23px] tracking-[-0.07em] font-medium">
+            Doubles: $145
+          </div>
+          <div className="absolute top-[192px] left-[138px] rounded-3xs bg-seagreen w-[126px] h-10" />
+          <div className="absolute top-[201px] left-[164px] tracking-[-0.07em] font-medium font-lato text-white">
+            Book now
+          </div>
+          <div className="absolute top-[0px] left-[193px] rounded-3xs bg-indianred w-[127px] h-10" />
+          <div className="absolute top-[10px] left-[224px] text-[13.35px] tracking-[-0.01em] font-medium text-white">
+            Rare Find
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[294px] left-[414px] rounded-3xl box-border w-[1072px] h-12 border-[1px] border-solid border-darkgray" />
+      <img
+        className="absolute top-[859px] left-[1550px] w-[138px] h-[138px]"
+        alt=""
+        src="/group-10.svg"
+      />
+    </div>
+  );
+};
+
+export default Request1;
